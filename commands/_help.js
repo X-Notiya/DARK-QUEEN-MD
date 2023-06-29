33fd3f04260c1f938bf1f17f6dbf3a7a0d47f9e5
X-Notiya <137847749+X-Notiya@users.noreply.github.com>
2023-06-29 20:49:04 +0530

    🦑⃝🧚‍♀️CHAMOD-MD🦑⃝🧚‍♀️

--- commands/_help.js
+++ commands/_help.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : CHAMOD-MD
+ * @author : X-Notiya <https://github.com/X-Notiya>
+ * @description : X-Notiya,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -24,7 +24,7 @@ Secktor.cmd({
             alias: ["menu"],
             desc: "Help list",
             category: "general",
-            react: "✨",
+            react: "🧚‍♂️",
             filename: __filename
         },
         async(Void, citel, text) => {
