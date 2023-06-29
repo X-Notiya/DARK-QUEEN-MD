c29172c532bdb765733261c266648794960d7411
X-Notiya <137847749+X-Notiya@users.noreply.github.com>
2023-06-29 20:52:40 +0530

    🦑⃝🧚‍♀️CHAMOD-MD🦑⃝🧚‍♀️

--- commands/_plugins.js
+++ commands/_plugins.js
@@ -3,8 +3,8 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
+ * @project_name : CHAMOD-MD
+ * @author : X-Notiya <https://github.com/X-Notiya>
  * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
@@ -41,7 +41,7 @@ cmd({
         if (!isCreator) return citel.reply(tlang().owner)
         if(text==='all') {
          await plugindb.collection.drop()
-         return citel.reply('Deleted all plugins from Secktor.')
+         return citel.reply('Deleted all plugins from X-Notiya.')
         }
         let kill = await remove(text.split(" ")[0])
         delete require.cache[require.resolve(__dirname+"/" + text + ".js")];
