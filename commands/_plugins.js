0190d2613589c532be5fa604f0220801f6c5f668
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-07-12 14:02:36 +0530

    🦑⃝🧚‍♀️CHAMOD-MD🦑⃝🧚‍♀️

--- commands/_plugins.js
+++ commands/_plugins.js
@@ -3,8 +3,8 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : CHAMOD-MD
- * @author : X-Notiya <https://github.com/X-Notiya>
+ * @project_name : Secktor-Md
+ * @author : SamaPndey001 <https://github.com/SamaPndey001>
  * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
