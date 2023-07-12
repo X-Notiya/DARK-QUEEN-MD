5da5ba3a46bf3a46ad8813d43c76b60f67659060
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-07-12 14:01:14 +0530

    ❑⃝🧚‍♂️DARK QUEEN❑⃝🧚‍♂️

--- commands/_help.js
+++ commands/_help.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : CHAMOD-MD
- * @author : X-Notiya <https://github.com/X-Notiya>
- * @description : X-Notiya,A Multi-functional whatsapp bot.
+ * @project_name : Secktor-Md
+ * @author : X-Notiya <https://github.com/SamaPndey001>
+ * @description : Secktor-Md Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
