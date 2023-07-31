550936f5f42158a745c60b806a500c62d410a95f
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-07-31 14:55:28 +0530

    💕⃢🧚‍♂️𝐃𝚫𝚪𝐊 𝐐𝐔𝚵𝚵𝚴 𝚳𝐃🧚‍♂️⃢ 💕

--- config.js
+++ config.js
@@ -7,20 +7,20 @@ global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'sam@secktor.live'
-global.github = 'https://github.com/X-Notiya/CHAMOD-MD'
+global.github = 'https://github.com/X-Notiya/DARK-QUEEN-MD'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.sudo = process.env.SUDO || '94715491788'
 global.devs = '94715491788';
-global.website = 'https://github.com/X-Notiya/Secktor-Md' //wa.me/+91000000000000
+global.website = 'https://github.com/X-Notiya/DARK-QUEEN-MD' //wa.me/+94715491788
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? '🦑⃝🧚‍♀️CHAMOD-MD🦑⃝🧚‍♀️' : process.env.BOT_NAME,
+  botname:   process.env.BOT_NAME === undefined ? 'DARK-QUEEN-MD' : process.env.BOT_NAME,
   ownername: process.env.OWNER_NAME === undefined ? 'X-Notiya' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '🦑⃝🧚‍♀️CHAMOD-MD🦑⃝🧚‍♀️' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'X-Notiya' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '🦑⃝🧚‍♀️CHAMOD-MD🦑⃝🧚‍♀️' : process.env.PACK_INFO.split(";")[1],
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'DARK-QUEEN-MD' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
