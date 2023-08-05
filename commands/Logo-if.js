f1528119a17bdcf3baab8b86da19ac71ff62aafa
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-08-05 08:33:20 +0530

    💕⃢🧚‍♂️𝐃𝚫𝚪𝐊 𝐐𝐔𝚵𝚵𝚴 𝚳𝐃🧚‍♂️⃢ 💕

--- commands/Logo-if.js
+++ commands/Logo-if.js
@@ -1,21 +1,16 @@
 /**
 
- Copyright (C) 2022.
+█▀█ █░█ █▀▀ █▀▀ █▄░█   █▄░█ █▀▀ ▀█▀ █░█ █░█   █▀▄▀█ █▀▄   █▄▄ █▀█ ▀█▀
+▀▀█ █▄█ ██▄ ██▄ █░▀█   █░▀█ ██▄ ░█░ █▀█ █▄█   █░▀░█ █▄▀   █▄█ █▄█ ░█░
 
+ Copyright (C) 2023.
  Licensed under the  GPL-3.0 License;
-
  You may not use this file except in compliance with the License.
-
  It is supplied in the hope that it may be useful.
-
- * @project_name : Secktor-Md
-
- * @author : SamPandey001 <https://github.com/SamPandey001>
-
- * @description : Secktor,A Multi-functional whatsapp bot.
-
+ * @project_name : QueenNethu-Md
+ * @author : MR.Hansamala <https://github.com/mrhansamala>
+ * @description : Nethu,A Multi-functional whatsapp bot.
  * @version 0.0.6
-
  **/
 
 const { tlang,cmd } = require('../lib')
