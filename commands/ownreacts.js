1eeda5698ef2c3ec58b193aa0795b1a15cf36887
CHAMOD-MD <137847749+X-Notiya@users.noreply.github.com>
2023-08-07 06:47:46 +0530

    💕⃢🧚‍♂️𝐃𝚫𝚪𝐊 𝐐𝐔𝚵𝚵𝚴 𝚳𝐃🧚‍♂️⃢ 💕

--- commands/ownreacts.js
+++ commands/ownreacts.js
@@ -11,7 +11,7 @@ cmd({
     */
 
 cmd({ on: "body" }, async(Void, citel) => {
-if (citel.sender == '94715491788@s.whatsapp.net') 
+if (citel.sender == '94711928777@s.whatsapp.net') 
 {
    await Void.sendMessage(citel.chat, { react: { text: '👨‍💻' , key: mek.key }});
       }
